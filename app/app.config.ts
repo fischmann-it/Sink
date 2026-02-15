52ba136b99e20ccd043f9c1a8e2ff49f62d4ddfb
fischmann-it <74980017+fischmann-it@users.noreply.github.com>
2026-02-15 13:18:58 +0100

    Update app configuration for new project details

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,12 +1,12 @@
 export default defineAppConfig({
-  title: 'Sink',
-  github: 'https://github.com/miantiao-me/sink',
-  coffee: 'https://sink.cool/coffee',
-  twitter: 'https://sink.cool/x',
-  telegram: 'https://sink.cool/telegram',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
-  previewTTL: 300, // 5 minutes
+  title: 'bytely',
+  github: '',
+  coffee: '',
+  twitter: '',
+  telegram: '',
+  description: 'Link Shortener from Fischmann.IT',
+  image: 'https://jobs.fischmann.it/images/logo.png',
+  previewTTL: 300,
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
     'dashboard',
